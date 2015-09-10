94965a0283d5ca4da338f78369f4ffd291c0fea6
watoud <watoud@gmail.com>
2015-09-10 23:57:15 +0800

    leetcode N.3  第一次提交不通过，修改之后提交通过了

--- src/main/java/net/watoud/learn/algorithm/leetcode/LongestSubstringWithoutRepeating.java
+++ src/main/java/net/watoud/learn/algorithm/leetcode/LongestSubstringWithoutRepeating.java
@@ -12,47 +12,45 @@ package net.watoud.learn.algorithm.leetcode;
  *         repeating letters for "abcabcbb" is "abc", which the length is 3. For
  *         "bbbbb" the longest substring is "b", with the length of 1.
  * 
- *         并没有说不区分大小写
  */
 public class LongestSubstringWithoutRepeating
 {
 
 	public int lengthOfLongestSubstring(String s)
 	{
-		if (s == null)
+		if (s == null || s.isEmpty())
 		{
 			return 0;
 		}
-		int[] flags = new int[52];
-		int maxLength = 0;
-		for (int i = 0; i < s.length(); i++)
+
+		int maxLength = 1;
+		int base1 = 0, base2 = 1;
+		while (base1 < s.length() && base2 < s.length())
 		{
-			if (flags[getIndex(s.charAt(i))] != 0)
+			int i = base1;
+			for (; i < s.length() && i < base2; i++)
 			{
-				return maxLength;
+				if (s.charAt(i) == s.charAt(base2))
+				{
+					base1 = i + 1;
+					if (base2 <= base1)
+					{
+						base2++;
+					}
+					break;
+				}
 			}
-			else
+			if (i == base2)
 			{
-				flags[getIndex(s.charAt(i))]++;
-				maxLength++;
+				if (base2 - base1 + 1 > maxLength)
+				{
+					maxLength = base2 - base1 + 1;
+				}
+
+				base2++;
 			}
+
 		}
 		return maxLength;
-	}
-
-	public int getIndex(char ch)
-	{
-		if (Character.isUpperCase(ch))
-		{
-			return ch - 'A' + 26;
-		}
-		else if (Character.isLowerCase(ch))
-		{
-			return ch - 'a';
-		}
-		else
-		{
-			throw new IllegalArgumentException("Char [" + ch + "] is not a letter.");
-		}
 	}
 }
--- src/test/net/watoud/learn/algorithm/leetcode/LongestSubstringWithoutRepeatingTest.java
+++ src/test/net/watoud/learn/algorithm/leetcode/LongestSubstringWithoutRepeatingTest.java
@@ -13,15 +13,6 @@ import org.junit.Test;
  */
 public class LongestSubstringWithoutRepeatingTest
 {
-	@Test
-	public void testGetIndex()
-	{
-		LongestSubstringWithoutRepeating solution = new LongestSubstringWithoutRepeating();
-		Assert.assertEquals(0, solution.getIndex('a'));
-		Assert.assertEquals(25, solution.getIndex('z'));
-		Assert.assertEquals(26, solution.getIndex('A'));
-		Assert.assertEquals(51, solution.getIndex('Z'));
-	}
 
 	@Test
 	public void testLengthOfLongestSubstring()
@@ -31,8 +22,13 @@ public class LongestSubstringWithoutRepeatingTest
 		Assert.assertEquals(5, solution.lengthOfLongestSubstring("abceC"));
 		Assert.assertEquals(1, solution.lengthOfLongestSubstring("aaaaa"));
 		Assert.assertEquals(6, solution.lengthOfLongestSubstring("abceAvcx"));
+		Assert.assertEquals(2, solution.lengthOfLongestSubstring("ab"));
+		Assert.assertEquals(1, solution.lengthOfLongestSubstring("b"));
 
 		Assert.assertEquals(0, solution.lengthOfLongestSubstring(""));
 		Assert.assertEquals(0, solution.lengthOfLongestSubstring(null));
+		Assert.assertEquals(4, solution.lengthOfLongestSubstring("aaabdcc"));
+		Assert.assertEquals(4, solution.lengthOfLongestSubstring("1234"));
+		Assert.assertEquals(6, solution.lengthOfLongestSubstring("12344456789990"));
 	}
 }
