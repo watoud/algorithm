025efbc15467884e858809796b2807d9eed26a6a
watoud <watoud@gmail.com>
2016-09-03 11:39:29 +0800

    Pascal's Triangle II

---
+++ src/main/java/net/watoud/learn/algorithm/leetcode/PascalTriangleII.java
@@ -0,0 +1,35 @@
+package net.watoud.learn.algorithm.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PascalTriangleII
+{
+	public List<Integer> getRow(int rowIndex)
+	{
+		if (rowIndex < 0)
+		{
+			return new ArrayList<>();
+		}
+
+		List<Integer> result = new ArrayList<>();
+		result.add(1);
+
+		for (int i = 1; i <= rowIndex; i++)
+		{
+			getRowAux(result);
+		}
+
+		return result;
+	}
+
+	private void getRowAux(List<Integer> r)
+	{
+		r.add(1);
+		for (int i = r.size() - 2; i > 0; i--)
+		{
+			r.set(i, r.get(i) + r.get(i - 1));
+		}
+	}
+
+}
---
+++ src/test/net/watoud/learn/algorithm/leetcode/PascalTriangleIITest.java
@@ -0,0 +1,27 @@
+package net.watoud.learn.algorithm.leetcode;
+
+import org.junit.Test;
+
+public class PascalTriangleIITest
+{
+	@Test
+	public void testGetRow()
+	{
+		PascalTriangleII sol = new PascalTriangleII();
+
+		System.out.println(sol.getRow(0));
+		System.out.println(sol.getRow(1));
+		System.out.println(sol.getRow(2));
+		System.out.println(sol.getRow(3));
+		System.out.println(sol.getRow(4));
+		System.out.println(sol.getRow(5));
+		System.out.println(sol.getRow(6));
+		System.out.println(sol.getRow(7));
+		System.out.println(sol.getRow(8));
+		System.out.println(sol.getRow(9));
+		System.out.println(sol.getRow(12));
+		System.out.println(sol.getRow(13));
+		System.out.println(sol.getRow(31));
+
+	}
+}
