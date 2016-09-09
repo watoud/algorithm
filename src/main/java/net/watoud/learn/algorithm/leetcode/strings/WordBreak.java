c88b06a3ad17bd616202cd9e224b8b3d3324332e
watoud <watoud@gmail.com>
2016-09-10 01:29:42 +0800

    Word Break 由递归算法改为动态规划算法

--- src/main/java/net/watoud/learn/algorithm/leetcode/strings/WordBreak.java
+++ src/main/java/net/watoud/learn/algorithm/leetcode/strings/WordBreak.java
@@ -4,8 +4,44 @@ import java.util.Set;
 
 public class WordBreak
 {
-	// 耗时较长
 	public boolean wordBreak(String s, Set<String> wordDict)
+	{
+		if (s == null || s.length() == 0)
+		{
+			return true;
+		}
+
+		int maxLength = Integer.MIN_VALUE;
+		for (String word : wordDict)
+		{
+			maxLength = Math.max(maxLength, word.length());
+		}
+
+		boolean[] ret = new boolean[s.length() + 1];
+		ret[0] = true;
+
+		for (int i = 1; i <= s.length(); i++)
+		{
+			for (int j = maxLength; j > 0; j--)
+			{
+				if (i - j < 0 || !ret[i - j])
+				{
+					continue;
+				}
+
+				if (wordDict.contains(s.substring(i - j, i)))
+				{
+					ret[i] = true;
+					break;
+				}
+			}
+		}
+
+		return ret[s.length()];
+	}
+
+	// 耗时较长
+	public boolean wordBreak_recurence(String s, Set<String> wordDict)
 	{
 		if (s == null || s.length() == 0)
 		{
@@ -14,11 +50,12 @@ public class WordBreak
 
 		for (String word : wordDict)
 		{
-			if (s.startsWith(word) && wordBreak(s.substring(word.length()), wordDict))
+			if (s.startsWith(word) && wordBreak_recurence(s.substring(word.length()), wordDict))
 			{
 				return true;
 			}
 		}
 		return false;
 	}
+
 }
